9df4b1adbe7af73ce7799d4d6ffbbf9c4c32bd2b
Jorge Lopez <georgeilr99@gmail.com>
2021-03-15 21:14:13 -0600

    unSubcribe clothes

--- src/app/modules/home/components/clothes/clothes.component.ts
+++ src/app/modules/home/components/clothes/clothes.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ClothesService } from 'src/app/shared/services/clothes.service';
+import { HomeComponent } from '../../home.component';
 
 @Component({
   selector: 'app-clothes',
@@ -15,23 +16,32 @@ export class ClothesComponent implements OnInit, OnDestroy, OnChanges {
   @Input()
   clotheType : string;
 
-  constructor(private router:Router ,private service: ClothesService) { }
+  constructor(private router:Router ,private service: ClothesService, private home: HomeComponent) { }
 
   ngOnInit(): void {
-    console.log('ngOnInit');
   }
 
-  ngOnChanges(){
-    console.log('changeee');
+  ngOnChanges(): void{
+    this.unSusbcribe();
+    this.getClothes();
+  }
+
+  
+  ngOnDestroy(): void{
+    this.unSusbcribe();
+    this.home.clothes=false;
+  }
+
+  getClothes(): void{
     this.service.setClotheType(this.clotheType);
     this.clothesSubs = this.service.get().subscribe(res => {
       Object.entries(res).map(p => this.clothes.push(p[1]));
     });
   }
 
-  ngOnDestroy(){
-    console.log('destroyyyyy');
+  unSusbcribe(): void{
     this.clothesSubs ? this.clothesSubs.unsubscribe():'';
   }
+
 
 }
